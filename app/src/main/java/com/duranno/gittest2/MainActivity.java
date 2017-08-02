b4af13c1c45905acd3b73280a466aca1b3309074
CescJuno <nallayo@nate.com>
2017-08-02 13:14:35 +0900

    test update

--- app/src/main/java/com/duranno/gittest2/MainActivity.java
+++ app/src/main/java/com/duranno/gittest2/MainActivity.java
@@ -2,6 +2,7 @@ package com.duranno.gittest2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        Log.d("","test");
 
 
 
